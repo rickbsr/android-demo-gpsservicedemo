fa1d8764d8ce38493509a22d5ff0fc13ae4ac4b9
Rick <rick8220602@gmail>
2019-01-30 14:27:28 +0800

    + firebase

--- app/src/main/java/com/codingbydumbbell/gpsservicedemo/userInfo/UserInfo.java
+++ app/src/main/java/com/codingbydumbbell/gpsservicedemo/userInfo/UserInfo.java
@@ -2,7 +2,7 @@ package com.codingbydumbbell.gpsservicedemo.userInfo;
 
 public class UserInfo {
 
-    public static String userUid;
+    public static String userUid = "";
 
     public static double originLng;
     public static double originLat;
